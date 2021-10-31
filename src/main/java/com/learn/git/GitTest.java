769afffdcbc5f35a5de0dda072ee63f3fd6197c9
auther <authertam1106@yeah.net>
2021-10-31 14:09:26 +0800

    master commit

--- src/main/java/com/learn/git/GitTest.java
+++ src/main/java/com/learn/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello gtihub");
         System.out.println("hello gtihub3");
         System.out.println("hello gtihub4");
+        System.out.println("hello master test");
     }
 }
